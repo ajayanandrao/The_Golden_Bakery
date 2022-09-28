274de5f31c21e1e0fb543edd61bb234cad179fba
Ajay Anandrao <32159029+ajayanandrao@users.noreply.github.com>
2022-09-28 15:51:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import PageNotFound from './Page note Found/PageNotFound';
 import AllinOne from './components/inner Componenets/AllinOne';
 import Navbar from './components/Navbar';
 import MainCart from './components/inner Componenets/MainCart';
+import ScrollToTop from './ScrollTop';
 
 function App() {
   return (
     <>
       <Router basename='The_Golden_Bakery'>
+        <ScrollToTop/>
         <Navbar />
         <Routes>
           <Route exact path='/' element={<Home />} />
